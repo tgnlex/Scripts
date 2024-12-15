59d8b0197de6488b7524fec3bbd3a3d09da62f65
tgnlex <alexanderwebber76@gmail.com>
2024-12-15 08:04:44 -0500

    moved a bunch of files to libraries repo

--- python/data/classes/node.py
+++
@@ -1,6 +0,0 @@
-class Node:
-  def __init(self,x_coord,y_coord):
-    self.d=float('inf')
-    self.parent=None
-    self.finished=False
-    
--- python/data/sort/insertion_sort.py
+++
@@ -1,8 +0,0 @@
-def insertion_sort_alg(list):
-  for i in range(1, len(list)):
-    key = list[i]
-    j = i - 1
-    while j >= 0 and list[j] > key:
-      list[j - 1] = list[j]
-      i -= 1
-    list[j + 1] = key
